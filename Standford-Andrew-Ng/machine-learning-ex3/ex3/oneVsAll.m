5306072849341356253c58c8d9afa51ddb398e4c
georgezhyuan <yuangeorge9981@gmail.com>
2017-10-01 02:39:35 +0100

    changed fminunc to fmincg for onevsall classification

--- Standford-Andrew-Ng/machine-learning-ex3/ex3/oneVsAll.m
+++ Standford-Andrew-Ng/machine-learning-ex3/ex3/oneVsAll.m
@@ -52,13 +52,10 @@ X = [ones(m, 1) X];
 for c = 1:num_labels
   initial_theta = zeros(n + 1, 1);
   options = optimset('GradObj', 'on', 'MaxIter', 50);
-  [initial_theta] = fminunc(@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
+  [initial_theta] = fmincg(@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
   all_theta(c,:) = initial_theta;
 endfor
-
-
-
-
+ 
 % =========================================================================
 
 
