bbf459b520826ddb6096c56ca926ea067227007e
georgezhyuan <yuangeorge9981@gmail.com>
2017-11-09 11:27:06 -0700

    implemented regularized cost function and gadient descent

--- Standford-Andrew-Ng/machine-learning-ex5-2/ex5/linearRegCostFunction.m
+++ Standford-Andrew-Ng/machine-learning-ex5-2/ex5/linearRegCostFunction.m
@@ -19,9 +19,11 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+reg_theta = theta;
+reg_theta(1,:) = 0
 
-
-
+J = (sum((X*theta-y).^2)+lambda*sum(reg_theta.^2))/(2*m);
+grad = (X'*(X*theta-y) + lambda*reg_theta)/m
 
 
 
